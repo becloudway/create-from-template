d39fc6ea9b314e59d256ad1458b69c3641a2133c
Stijn De Pestel <stijn.depestel@gmail.com>
2023-01-25 11:15:34 +0100

    Build script fix where libraries were not correctly included.

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -9,6 +9,7 @@ import nodeExternals from 'webpack-node-externals';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 import { $ } from 'zx';
+import { createRequire } from 'node:module';
 
 const { npm_package_name: npmPackageName, PROJECT_CWD, npm_package_version: npmPackageVersion } = process.env;
 
@@ -20,13 +21,23 @@ const logger = winston.createLogger({
   format: winston.format.combine(winston.format.simple(), winston.format.cli()),
 });
 
-const webpackConfiguration = {
+const getLibraryNames = async () => {
+  const libFolderNames = (await $`ls ${PROJECT_CWD}/libraries`).stdout.split(/\s+/).filter((s) => s.length);
+  const reqre = createRequire(`${PROJECT_CWD}/libraries`);
+  const libNames = libFolderNames.map((f) => reqre(`${PROJECT_CWD}/libraries/${f}/package.json`).name);
+  logger.verbose(`Found library folders ${libNames}`);
+  return libNames;
+};
+
+const webpackConfiguration = (libNames) => ({
   mode: 'production',
   entry: './src/main.ts',
+  externalsPresets: { node: true },
   externals: [
     nodeExternals(),
     nodeExternals({
       modulesDir: `${PROJECT_CWD}/node_modules`,
+      allowlist: (name) => libNames.includes(name),
     }),
   ],
   resolve: {
@@ -49,12 +60,13 @@ const webpackConfiguration = {
   output: {
     path: bundleFolder,
     filename: 'index.bundle.js',
+    clean: true,
   },
-};
+});
 
-const webpackPromise = () =>
+const webpackPromise = (config) =>
   new Promise((accept, reject) => {
-    webpack(webpackConfiguration, (err, stats) => {
+    webpack(config, (err, stats) => {
       if (err) {
         logger.error((err.stack && err.stack.toString()) || err);
         if (err.details) {
@@ -80,7 +92,8 @@ const webpackPromise = () =>
 const script = async (flags) => {
   logger.info(`Building package ${npmPackageName}@${npmPackageVersion}`);
   logger.verbose(`Using flags ${JSON.stringify(flags)}`);
-  await webpackPromise();
+  const libNames = await getLibraryNames();
+  await webpackPromise(webpackConfiguration(libNames));
 
   const packageNameForDocker = npmPackageName.replace('@', '');
   await $`docker build -f ${PROJECT_CWD}/Dockerfile --no-cache --build-arg BUNDLE_FOLDER=${bundleFolderRelative} --build-arg WORKSPACE_NAME=${npmPackageName} --force-rm --pull -t ${packageNameForDocker}:${npmPackageVersion} -t ${packageNameForDocker}:latest ${PROJECT_CWD}`;
